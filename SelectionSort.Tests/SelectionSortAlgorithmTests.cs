03ffee206ea1bde6e7ac36aa99a943378e2ea44b
Marcin Siemiński <marcin.sieminski@outlook.com>
2023-05-28 17:45:04 +0200

    Implement selection sort algorithm

--- SelectionSort.Tests/SelectionSortAlgorithmTests.cs
+++ SelectionSort.Tests/SelectionSortAlgorithmTests.cs
@@ -9,7 +9,36 @@ public class SelectionSortAlgorithmTests
         var sut = new SelectionSort();
 
         var result = sut.Sort(input);
-        
+
         Assert.Equal(result, input);
+    }
+
+    [Theory]
+    [InlineData(new[] { 0, -1 })]
+    [InlineData(new[] { 2, 1 })]
+    [InlineData(new[] { 10, 20 })]
+    public void Given2ElementArray_ShouldReturnSorted2ElementArray(int[] input)
+    {
+        var sut = new SelectionSort();
+
+        var result = sut.Sort(input);
+
+        Assert.Equal(2, result.Length);
+        Assert.True(result[0] < result[1]);
+    }
+
+    [Theory]
+    [InlineData(new[] { 2, 1, 52, -400, 12 })]
+    [InlineData(new[] { 10, 20, 111, 0, 0, 1, -5, -45, -55, 1111 })]
+    public void GivenAnyArray_ShouldReturnSortedArray(int[] input)
+    {
+        var sut = new SelectionSort();
+
+        var result = sut.Sort(input);
+
+        for (int i = 0; i < result.Length - 1; i++)
+        {
+            Assert.True(result[i] <= result[i + 1]);
+        }
     }
 }
--- SelectionSort/SelectionSort.cs
+++ SelectionSort/SelectionSort.cs
@@ -4,13 +4,28 @@ public class SelectionSort
 {
     public T[] Sort<T>(T[] array) where T : IComparable
     {
-        var sortedArray = new T[array.Length];
-
-        if (array.Length == 1)
+        for (int i = 0; i < array.Length - 1; i++)
         {
-            sortedArray = array;
+            int minIndex = i;
+            T minValue = array[i];
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j].CompareTo(minValue) < 0)
+                {
+                    minIndex = j;
+                    minValue = array[j];
+                }
+
+            }
+            Swap(array, i, minIndex);
         }
-        
-        return sortedArray;
+        return array;
+    }
+
+    private void Swap<T>(T[] array, int first, int second) where T : IComparable
+    {
+        T temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
     }
 }

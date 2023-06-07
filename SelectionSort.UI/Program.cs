1f1733474020f6c69a763d56b96ad218a81865bb
Marcin Siemiński <marcin.sieminski@outlook.com>
2023-06-07 22:01:49 +0200

    Minor improvements

--- SelectionSort.UI/Program.cs
+++ SelectionSort.UI/Program.cs
@@ -10,7 +10,7 @@ do
     Console.WriteLine("Aplikacja demonstracyjna algorytmu sortowania przez wybieranie");
     Console.WriteLine("\nProszę wybrać opcję:\n");
     Console.WriteLine("1. Przykład sortowania na tablicy liczb całkowitych");
-    Console.WriteLine("2. Przykład sortowania na tablicy liczb rzeczywistych");
+    Console.WriteLine("2. Przykład sortowania na tablicy liczb zmiennoprzecinkowych");
     Console.WriteLine("3. Przykład sortowania na tablicy ciągów znakowych");
     Console.WriteLine("4. Uruchomienie testów");
     Console.WriteLine("5. Powrót do systemu");
@@ -22,41 +22,41 @@ do
     {
         case '1':
             Console.Clear();
-            Console.WriteLine("Przykład sortowania na tablicy liczb całkowitych.");
-            var arrayInt = new[] { 5, 2, 0 };
+            Console.WriteLine("Przykład sortowania na tablicy liczb całkowitych");
+            var arrayInt = new[] { 5, 2, 0, 999, -1000, Int32.MaxValue, Int32.MinValue };
             Console.WriteLine("\nPrzed sortowaniem:");
             PrintItems(arrayInt);
             Console.WriteLine();
             SelectionSortHelper.Sort(arrayInt);
             Console.WriteLine("\nPo sortowaniu:");
             PrintItems(arrayInt);
-            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować.\n");
+            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować");
             Console.ReadKey();
             break;
         case '2':
             Console.Clear();
-            Console.WriteLine("Przykład sortowania na tablicy liczb rzeczywistych.");
-            var arrayDouble = new[] { 155.5, -5656.02, 15656.22, -2000.0 };
+            Console.WriteLine("Przykład sortowania na tablicy liczb zmiennoprzecinkowych");
+            var arrayDouble = new[] { 155.5, -5656.02, 15656.22, -2000.0, 0.0, -999999999.999, Double.MaxValue,  Double.MinValue };
             Console.WriteLine("\nPrzed sortowaniem:");
             PrintItems(arrayDouble);
             Console.WriteLine();
             SelectionSortHelper.Sort(arrayDouble);
             Console.WriteLine("\nPo sortowaniu:");
             PrintItems(arrayDouble);
-            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować.");
+            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować");
             Console.ReadKey();
             break;
         case '3':
             Console.Clear();
-            Console.WriteLine("Przykład sortowania na tablicy ciągów znakowych.");
-            var arrayString = new[] { "Marek", "Anna", "Wioletta", "Zenon", "Grzegorz", "Jerzy", "Adam" };
+            Console.WriteLine("Przykład sortowania na tablicy ciągów znakowych");
+            var arrayString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".Replace(".", String.Empty).Replace(",", String.Empty).Split();
             Console.WriteLine("\nPrzed sortowaniem:");
             PrintItems(arrayString);
             Console.WriteLine();
             SelectionSortHelper.Sort(arrayString);
             Console.WriteLine("\nPo sortowaniu:");
             PrintItems(arrayString);
-            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować.");
+            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować");
             Console.ReadKey();
             break;
         case '4':
@@ -69,18 +69,18 @@ do
                 var process = Process.Start("dotnet", @$"test {path} --logger ""console;verbosity=detailed""");
                 process.WaitForExit();
             }
-            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować.");
+            Console.WriteLine("\n\nNaciśnij ENTER aby kontynuować");
             Console.ReadKey();
             break;
         case '5':
             Console.Clear();
-            Console.WriteLine("Wybrano powrót do systemu.");
+            Console.WriteLine("Wybrano powrót do systemu");
             continueRun = false;
             break;
         default:
             Console.Clear();
-            Console.WriteLine("Wybrano nieprawidłową opcję.");
-            Console.WriteLine("Naciśnij ENTER aby kontynuować.");
+            Console.WriteLine("Wybrano nieprawidłową opcję");
+            Console.WriteLine("Naciśnij ENTER aby kontynuować");
             Console.ReadKey();
             break;
     }
@@ -88,16 +88,6 @@ do
 
 void PrintItems<T>(T[] array)
 {
-    Console.Write("[ ");
-    for (var index = 0; index < array.Length; index++)
-    {
-        var item = array[index];
-        Console.Write($"{item}");
-        if (index < array.Length - 1)
-        {
-            Console.Write(" | ");
-        }
-    }
-    Console.WriteLine(" ]");
+    Console.WriteLine($"[ {string.Join(" | ", array)} ]");
 }
 
